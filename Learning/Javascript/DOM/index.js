167301d72af8b185054f72e7dd0e54eab64dea0f
Navansh <khandelwalnavansh1@gmail.com>
2023-03-04 09:43:50 +0530

    do

--- Learning/Javascript/DOM/index.js
+++ Learning/Javascript/DOM/index.js
@@ -1,12 +1,14 @@
 let MyDiv = document.createElement('div');
+function paraStatus(event){
+   console.log('Para:' + event.target.textContent);
+   //hence through this we targetted the individual p tags through 'target' method
+}
+MyDiv.addEventListener('click',paraStatus)
 
 for (let i = 0; i <= 100; i++) {
      let newElement = document.createElement('p');
      newElement.textContent = 'This is para'+ i;
-
-     newElement.addEventListener('click',function(event){
-        console.log('I Have clicked on it');
-     })
+     
      MyDiv.appendChild(newElement);
 }
-document.appendChild(myDiv);
+document.body.appendChild(MyDiv); 
