f5d39d6ac36dadbc7ec05ce17cc34260cab26e24
Navansh <khandelwalnavansh1@gmail.com>
2023-05-10 18:23:12 +0530

    do

--- Learning/Projects/plan-with-love/src/App.js
+++ Learning/Projects/plan-with-love/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import data from './data'
 import { useState } from 'react';
@@ -7,13 +7,26 @@ import Tours from './components/Tours'
 function App() {
 
   const [tours,setTours] = useState(data);
+
+  function removeTour(id){
+      const newTours = tours.filter(tour => tour.id !== id);
+      setTours(newTours);
+  }
   
+  if(tours.length===0){
+    return(
+      <div className='refresh'>
+        <h2>No Tours Left</h2>
+        <button className='btn-white' onClick={()=> setTours(data)}>Refresh</button>
+      </div>
+    ); 
+  }
   return (
-    <div>
-      <h2>Tour IT</h2>
-      <Tours tours={tours}>
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}>
 
       </Tours>
+
     </div>
   );
 }
--- Learning/Projects/plan-with-love/src/components/Tours.js
+++ Learning/Projects/plan-with-love/src/components/Tours.js
@@ -1,7 +1,24 @@
 import './Tours.css';
+import Card from './Card'
 
-function Tours(){
-    
+function Tours({tours, removeTour}){
+    return( 
+        <div className='container'>
+            <div>
+                <h2 className='title'>TourIT</h2>
+            </div>
+            <div className='cards'>
+            {
+                tours.map((tour) =>{
+                    return <Card {...tour} removeTour={removeTour}></Card>
+                    //...tour se poore ke poore object ki copy pass ho gyi
+                })
+            } 
+            </div>
+
+        </div>
+    );
+
 }
 
 export default Tours;
