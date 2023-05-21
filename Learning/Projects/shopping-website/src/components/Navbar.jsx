8053e8a3380d26c2a540a3a93f7cb11a6485bf60
Navansh <khandelwalnavansh1@gmail.com>
2023-05-22 00:44:57 +0530

    do

---
+++ Learning/Projects/shopping-website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className=' flex justify-between'>
+            <img src="" alt='Logo'/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
