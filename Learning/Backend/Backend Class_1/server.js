bd1f617b020cc48d0ab787c5074dc844effe7783
Navansh <khandelwalnavansh1@gmail.com>
2023-05-27 17:16:01 +0530

    do

--- Learning/Backend/Backend Class_1/server.js
+++ Learning/Backend/Backend Class_1/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const app = express()
+//Server Instantiate
 
 const bodyParser = require('body-parser')
 //used to parse req.body in Express -> Specifically used in PUT or POST case
@@ -11,12 +11,15 @@ app.use(bodyParser.json())
 app.listen(3000, ()=>{
     console.log("Server Started at port 3000")
 })
+//activate the server on 3000 port
 
+//Now creating Routes : 
 app.get('/', (req,res) => {
     res.send("Yes Betaa, Dance !")
 
 })
 
+//PostMan Testing
 app.post('/api/cars', (request, response) => {
     //here request ke andar hi data hoga
     const {name,brand} = request.body;
@@ -24,3 +27,15 @@ app.post('/api/cars', (request, response) => {
     console.log(brand)
     response.send("Car Submitted Successfully")
 })
+
+//Mongoose Connection for linking MongoDB with Express
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/Test',{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    console.log("Connection Successful")
+}).catch((error) => {
+    console.log("Haan bhai error aa gya")
+})
+//"test" is the name of the database, if its not created before then it will automatically be created 
