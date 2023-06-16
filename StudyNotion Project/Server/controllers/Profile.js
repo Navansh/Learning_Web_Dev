7f97af9d0bfb79b236682715d62b5d3fca8d7db7
Navansh <khandelwalnavansh1@gmail.com>
2023-06-16 10:22:10 +0530

    do

---
+++ StudyNotion Project/Server/controllers/Profile.js
@@ -0,0 +1,154 @@
+//profile need not be created as hamne ek dummy profile user ke signup ke time create kar di thi
+//initializing them with "" 
+//hence, now we need to just update them
+
+const Profile = require('../models/Profile');
+const User = require('../models/User');
+
+exports.updateProfile = async (req, res) => {
+    try {
+        //if the user is logged in, means by auth middleware, the request already has the decoded token
+        //and the token contains, email, accountType, userid
+        //we can use the userid to find the user in the database
+        const {dateOfBirth = "", about="", contactNumber, gender} = req.body;
+
+        const id = req.user.id;
+
+        //validating the data
+        if (!contactNumber || !gender) {
+            return res.status(400).json({
+                success: false,
+                message: 'Please fill all the fields',
+            });
+        }
+            
+        //updating the profile
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+
+        const updatedProfile = await Profile.findByIdAndUpdate(
+            {_id : profileId},
+            {
+                dateOfBirth,
+                about,
+                contactNumber,
+                gender
+            },
+            {new : true}
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: 'Profile updated successfully',
+            data: updatedProfile,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
+
+//delete account
+exports.deleteAccount = async (req, res) => {
+    try {
+        const id = req.user.id;
+
+        //validation
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+
+        if(!userDetails || !profileId){
+            return res.status(400).json({
+                success: false,
+                message: 'User not found',
+            });
+        }
+
+        //delete the profile
+        await Profile.findByIdAndDelete(profileId);
+
+        //now unenroll the user from all the courses
+        //get the courses enrolled by the user
+        const coursesEnrolled = userDetails.courses;
+        //for each course, find the user and remove it from the list
+        coursesEnrolled.forEach(async (courseId) => {
+            await Course.findByIdAndUpdate(
+                {_id : courseId},
+                {$pull : {usersEnrolled : id}},
+                {new : true} 
+            );
+        });
+
+
+        //finally, scheduling the deleting of the user after 3 days
+        //we are not deleting the user directly, because we want to give the user a chance to undo the deletion
+        //hence, we are scheduling the deletion after 3 days
+        //we are using setTimeout function to schedule the deletion
+
+        //first, we need to get the user again, because we need the createdAt field
+
+        setTimeout(async () => {
+            //get the user again
+            const user = await User.findById(id);
+
+            //if the user is null, means the user has already been deleted
+            //hence, we need not do anything
+            if(!user){
+                return;
+            }
+
+            //if the user is not null, means the user has not been deleted
+            //hence, we need to delete the user
+            await User.findByIdAndDelete(id);
+
+        }, 3 * 24 * 60 * 60 * 1000); //3 days
+
+
+        return res.status(200).json({
+            success: true,
+            message: 'Account deleted successfully',
+            data: deletedUser,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
+
+
+exports.getAllUserDetails = async (req, res) => {
+    try {
+        //get the id of the user
+        const id = req.user.id;
+
+        //find the user details
+        const userDetails = await User.findById(id).populate('additionalDetails').exec();
+
+        //if no user found
+        if(!userDetails){
+            return res.status(400).json({
+                success: false,
+                message: 'No user found',
+            });
+        }
+
+        //if users found
+        return res.status(200).json({
+            success: true,
+            message: 'User found',
+            data: userDetails,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
--- StudyNotion Project/Server/controllers/SubSection.js
+++ StudyNotion Project/Server/controllers/SubSection.js
@@ -22,8 +22,7 @@ exports.createSubSection = async (req, res) => {
 
         const uploadDetails = await uploadImageToCloudinary(video, process.env.FOLDER_NAME)
 
-        videoUrl = uploadDetails.secure_url;
-
+        const videoUrl = uploadDetails.secure_url;
 
         //subSection create
         const newSubSection = await SubSection.create({
@@ -63,4 +62,94 @@ exports.createSubSection = async (req, res) => {
 }
 
 //update SubSection
+exports.updateSubSection = async (req, res) => {
+    try {
+        const {newTitle, newTimeDuration, newDescription, subSectionId} = req.body;
+
+        const newVideo = req.files.videoFile;
+
+        //data validation
+        if(!newTitle || !newTimeDuration || !newDescription || !subSectionId || !newVideo){
+            return res.status(400).json({
+                success: false,
+                message: 'Please fill all the fields',
+            });
+        }
+
+        const uploadDetails = await uploadImageToCloudinary(newVideo, process.env.FOLDER_NAME)
+
+        const newVideoUrl = uploadDetails.secure_url;
+
+        //subSection update
+        const updatedSubSection = await SubSection.findByIdAndUpdate(
+                {_id : subSectionId},
+                {
+                    title : newTitle,
+                    timeDuration : newTimeDuration,
+                    description : newDescription,
+                    videoUrl : newVideoUrl,
+                },
+                {new : true}
+            );
+                
+        //return the response
+        return res.status(200).json({
+            success: true,
+            message: 'SubSection updated successfully',
+            data: updatedSubSection,
+        });
+
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });       
+    }
+}
+
 //delete SubSection
+exports.deleteSubSection = async (req, res) => {
+    try {
+        const {subSectionId} = req.body;
+
+        //data validation
+        if(!subSectionId){
+            return res.status(400).json({
+                success: false,
+                message: 'Please fill all the fields',
+            });
+        }
+
+        //subSection delete
+        const deletedSubSection = await SubSection.findByIdAndDelete({_id : subSectionId});
+
+        //update the section object with the subSection id and alsp populating the section and subsections
+        // const updatedSection = await Section.findByIdAndUpdate(
+        //         {_id : deletedSubSection.sectionId},
+        //         {
+        //             $pull : {
+        //                 subSections : deletedSubSection._id,
+        //             },
+        //         },
+        //         {new : true}
+        //     ).populate({
+        //         path : 'subSections',
+        //     }).exec();
+
+        //do we need to this ??, i.e. updating the section also ? 
+        //return the response
+        return res.status(200).json({
+            success: true,
+            message: 'SubSection deleted successfully',
+            data: updatedSection,
+        });
+
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });       
+    }
+}
